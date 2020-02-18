2f8fbbf92b843dc7ec42ec45cefc45106b6324f4
Maxim Yakovlev <miakovlev@luxoft.com>
2020-02-18 15:24:57 +0300

    page up down fix

--- main.c
+++ main.c
@@ -329,9 +329,12 @@ void editorProcessKeypress(void)
         case PAGE_UP:
         case PAGE_DOWN:
             {
-                int times = E.screenrows;
-                while (times--)
-                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                if (c == PAGE_UP) {
+                    E.cy = E.rowoff;
+                } else if (c == PAGE_DOWN) {
+                    E.cy = E.rowoff + E.screenrows - 1;
+                    if (E.cy > E.numrows) E.cy = E.numrows;
+                }
             }
             break;
         case HOME_KEY:
