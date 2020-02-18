38ae6d51b88a930a07c5331a571af90d6c611d79
Maxim Yakovlev <miakovlev@luxoft.com>
2020-02-18 17:31:57 +0300

    add basic statistic for status bar

--- main.c
+++ main.c
@@ -417,11 +417,27 @@ void editorDrawRows(struct abuf *ab)
 void editorDrawStatusBar(struct abuf *ab)
 {
     abAppend(ab, "\x1b[7m", 4); // reverse color( from black to white, for example )
-    int len = 0;
+    
+    char status[80],
+         rstatus[80];
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+        E.filename ? E.filename : "[No Name]", E.numrows);
+
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+        E.cy + 1, E.numrows);
+
+    if (len > E.screencols) len = E.screencols;
+    abAppend(ab, status, len);
+
     while (len < E.screencols) {
+        if (E.screencols - len == rlen) {
+            abAppend(ab, rstatus, rlen);
+            break;
+        }
         abAppend(ab, " ", 1); // fill last row with spaces
         len++;
     }
+    
     abAppend(ab, "\x1b[m", 3); // off color changes
 }
 void editorRefreshScreen(void)
