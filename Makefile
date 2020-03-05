f7aaaac6290792f44443b31b93293fae18b2d7ca
Maxim Yakovlev <miakovlev@luxoft.com>
2020-03-05 13:37:19 +0300

    setup build system, split folders, fix errors

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
-build: main.c
-	$(CC) main.c -o pged -Wall -Wextra -pedantic -std=c99
-test: main.c pged testFile.txt
-	$(CC) main.c -o pged -Wall -Wextra -pedantic -std=c99 && ./pged testFile.txt
+CCFLAGS  := -Wall -Wextra -pedantic -std=c99
+
+MAINS    := main.c
+TARGETS  := pged
+
+$(TARGETS): $(MAINS)
+	$(CC) $(MAINS) -o $(TARGETS) $(CCFLAGS)
+
+test: $(MAINS) $(TARGETS) testFile.txt
+	$(CC) $(MAINS) -o $(TARGETS) $(CCFLAGS) && ./$(TARGETS) testFile.txt
+
+clean:
+	rm -f $(TARGETS)
+
+install: $(TARGETS)
+	cp $(TARGETS) /usr/local/bin/$(TARGETS)
+uninstall:
+	rm -f $(TARGETS) /usr/local/bin/$(TARGETS)
--- main.c
+++ main.c
@@ -409,6 +409,29 @@ void editorSave()
     editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
+/** find **/
+
+
+void editorFind()
+{
+    char *query = editorPrompt("Search: %s (ESC to cancel)");
+    if (query == NULL) return;
+
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+        erow *row = &E.rows[i];
+        char *match = strstr(row->render, query);
+        if (match) {
+            E.cy = i;
+            E.cx = match - row->render;
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+
+    free(query);
+}
+
 /** append buffer **/
 
 struct abuf {
