724a49c973567ffdcfa1b3abbea5e5d534dcdbef
Maxim Yakovlev <miakovlev@luxoft.com>
2020-03-10 18:46:57 +0300

    add navigation to search

--- source/search.c
+++ source/search.c
@@ -5,19 +5,45 @@
 #include "pged.h"
 #include "user_io.h"
 #include "text_operations.h"
+#include "editor_core.h"
 
 void editorFindCallback(char *query, int key)
 {
+  static int last_match = -1;
+  /**
+   * directions
+   *  1 - down
+   * -1 - up
+   */
+  static int direction = 1;
+
   if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
     return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
+
+  if (last_match == -1) direction = 1;
+  int current = last_match;
   
   for (int i = 0; i < E.numrows; i++) {
-    erow *row = &E.rows[i];
+    current += direction;
+    if (current == -1) current = E.numrows - 1;
+    else if (current == E.numrows) current = 0;
+
+    erow *row = &E.rows[current];
     char *match = strstr(row->render, query);
 
     if (match) {
-      E.cy = i;
+      last_match = current;
+      E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
       break;
